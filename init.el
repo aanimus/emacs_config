76f028020d510f81ba31e1895480a6d3ca9f9a60
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2016-01-08 15:22:42 -0500

    added gui settings

--- init.el
+++ init.el
@@ -1,14 +1,31 @@
-;;;;BACKUP DIR
+;;;PATH
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setenv "PATH" (concat (getenv "PATH") ":/Applications/Julia-0.4.1.app/Contents/Resources/julia/bin/"))
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/Applications/Julia-0.4.1.app/Contents/Resources/julia/bin/")
+
+;;;BACKUP DIR
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
             `((".*" ,temporary-file-directory t)))
 
-;;;;PACKAGE MANAGER
+;;;PACKAGE MANAGER
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives
 	       '("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+(setq inhibit-splash-screen t)
 
+;;;GUI
+(load "~/.emacs.d/myGui.el")
+
+;;;KEYS END OF LINE, BEGIN OF LINE
+(global-set-key (kbd "<s-right>") 'end-of-line )
+(global-set-key (kbd "<s-left>") 'beginning-of-line )
+
+;;;brackets
+(setq c-default-style "linux"
+                c-basic-offset 4)
--- mygui.el
+++ mygui.el
@@ -1,13 +1,36 @@
+(set-frame-parameter nil 'fullscreen 'fullboth) ;fullscreen
+
+(global-linum-mode 1)
+(setq linum-format "%4d \u2502")
+(menu-bar-mode -1)
+
+(condition-case nil
+  (tool-bar-mode -1)
+  (error nil))
+(condition-case nil
+  (scroll-bar-mode -1)
+  (error nil))
+
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
-(custom-set-faces
- )
+    ("47ac4658d9e085ace37e7d967ea1c7d5f3dfeb2f720e5dec420034118ba84e17" default))
+   (quote
+    ("705f3f6154b4e8fac069849507fd8b660ece013b64a0a31846624ca18d6cf5e1" "47ac4658d9e085ace37e7d967ea1c7d5f3dfeb2f720e5dec420034118ba84e17" default))))
 
-(load-theme 'atom-one-dark t)
+;;;window system settings
+(when window-system
 
-(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
-(require 'powerline)
+  (load-theme 'atom-one-dark t)
 
+  (add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline/")
+  (require 'powerline)
 
+  (custom-set-faces
+   '(mode-line ((t (:foreground "#f9f9f9" :background "#6666e6" :box nil))))
+   '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666676" :box nil)))))
+  )
