98879be691818017acd62e12d906bd7ba84ab24a
Serge-Olivier Amega <sergeolivieramega@gmail.com>
2016-01-08 13:05:13 -0500

    first commit.

---
+++ init.el
@@ -0,0 +1,14 @@
+;;;;BACKUP DIR
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+            `((".*" ,temporary-file-directory t)))
+
+;;;;PACKAGE MANAGER
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives
+	       '("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+
---
+++ mygui.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ '(custom-safe-themes
+   (quote
+    ("4904daa168519536b08ca4655d798ca0fb50d3545e6244cefcf7d0c7b338af7e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))))
+(custom-set-faces
+ )
+
+(load-theme 'atom-one-dark t)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+(require 'powerline)
+
+
